42ac3750350f2d0496bcd5edf04835f2fae42376
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-03-16 08:38:58 +0600

    loading data using axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import AxiosNews from './Components/AxiosNews/AxiosNews';
+import RecipeReview from './Components/RecipeReview/RecipeReview';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 function App() {
+  const [articles, setArticles] = useState([])
+  useEffect(() =>{
+    const url = ('http://newsapi.org/v2/everything?q=apple&from=2021-03-05&to=2021-03-05&sortBy=popularity&apiKey=648a47f69b5046a590f3e4960a2e5418')
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+      {
+        articles.map(article => <AxiosNews article={article}></AxiosNews>)
+      }
+      {
+        articles.map(article => <RecipeReview article={article}></RecipeReview>)
+      }
     </div>
   );
 }
---
+++ src/Components/RecipeReview/RecipeReview.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// npm install @material-ui / icons // when i can upload icon use this commmand//
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import './RecipeReview.css'
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+}));
+
+const RecipeReview = (props) => {
+    const { author, description, urlToImage, publishedAt, content} = props.article;
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+    return (
+        <div className="cards col-md-2">
+            <Card className={classes.root}>
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label="recipe" className={classes.avatar}>
+                            Min
+          </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title={author}
+                    subheader={publishedAt}
+                />
+                <CardMedia
+                    className={classes.media}
+                    image={urlToImage}
+                    title="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      {description}
+        </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>Method:</Typography>
+                        <Typography paragraph>
+                            {content}
+          </Typography>
+                        <Typography paragraph>
+                            {content}
+          </Typography>
+                        <Typography paragraph>
+                            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                            again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                            minutes more. (Discard any mussels that don’t open.)
+          </Typography>
+                        <Typography>
+                            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card>
+        </div>
+    );
+};
+
+export default RecipeReview;
